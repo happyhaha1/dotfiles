a7382231d5c447f0e865f84f0327a8c8c2c0d82a
郑开心 <happyzhengkaixin@gmail.com>
2017-10-01 08:26:21 +0800

    change

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -47,18 +47,23 @@ set_config() {
     else
         git clone https://github.com/erikw/tmux-powerline.git $tmuxpowerline
     fi
+    cp -rf ~/dotfiles/z .config/fish/
     use_ln .yarnrc
     use_ln .npmrc
     use_ln .tmux.conf
     use_ln .ideavimrc
     use_ln .tmux-powerlinerc
+    use_ln .config/fish/config.fish
+    use_ln .config/fish/alias.fish
+    use_ln .config/fish/functions.fish
+    use_ln .gradle/gradle.properties
 }
 
 use_ln(){
     config=$HOME"/$1"
     if [ -f "$config" ]; then
       echo "$1 alreay ..."
-      mv $config $config".bak"
+    #   mv $config $config".bak"
     fi
     ln -s $DOTFILES_ROOT/$1 ~/$1
 }
