4689f2748dc6022ce0cb4530f306c294b90f6508
happyhaha1 <474248257@qq.com>
2026-02-04 10:45:20 +0800

    🐟 优化 Fish shell 配置
    Docker、npm、Python 等环境变量采用 XDG 规范；新增智能历史记录过滤：排除 ls、cd、git 等常用命令的重复记录，保留真正有意义的历史；fish_add_path 添加 node_modules/.bin

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -17,3 +17,112 @@ set -x XDG_MUSIC_DIR "$HOME/Music"
 set -x XDG_PICTURES_DIR "$HOME/Pictures"
 set -x XDG_VIDEOS_DIR "$HOME/Videos"
 
+# if test (uname) = Darwin
+#     eval "$(/opt/homebrew/bin/brew shellenv)"
+#     fish_add_path "/opt/homebrew/opt/llvm/bin/"
+# end
+
+# docker
+set -gx DOCKER_CONFIG "$XDG_CONFIG_HOME/docker"
+
+# hammerspoon
+# defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
+
+# hunspell
+# set -gx DICPATH "$XDG_CONFIG_HOME/hunspell/dictionaries"
+
+# keras
+# set -gx KERAS_HOME "$XDG_DATA_HOME/keras"
+
+# lean
+# set -gx ELAN_HOME "$XDG_DATA_HOME/elan"
+
+# less
+# set -gx LESSHISTFILE "$XDG_STATE_HOME/less/history"
+
+# man
+# set -x MANPAGER "nvim +Man!"
+
+# npm / node
+set -gx NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npm/npmrc"
+fish_add_path "./node_modules/.bin"
+
+# OCaml
+# set -gx OPAMROOT "$XDG_DATA_HOME/opam"
+
+# This adds: the correct directories to the PATH, auto-completion for the opam binary
+# test -r '/Users/sparkes/.share/opam/opam-init/init.fish' && source '/Users/sparkes/.share/opam/opam-init/init.fish' >/dev/null 2>/dev/null; or true
+
+# python
+set -gx PYTHONPYCACHEPREFIX "$XDG_CACHE_HOME/python_cache"
+set -gx PYTHON_HISTORY "$XDG_CACHE_HOME/python/history"
+set -gx IPYTHONDIR "$XDG_CACHE_HOME/ipython"
+set -gx MPLCONFIGDIR "$XDG_CACHE_HOME/matplotlib"
+
+# rust
+# source "$HOME/.cargo/env.fish"
+
+# etc
+
+## https://fishshell.com/docs/current/faq.html#faq-history
+function last_history_item
+    echo $history[1]
+end
+abbr -a !! --position anywhere --function last_history_item
+
+function fish_should_add_to_history
+    # Ignore all uses of the following
+    for cmd in man which ls cd z pkgconf pkg-config
+        string match -qr "^$cmd" -- "$argv"; and return 1
+    end
+
+    # Ignore specific cargo commands
+    if string match -qr "^cargo (init)" -- "$argv"
+        return 1
+    end
+
+    # Ignore specific git
+    if string match -qr "^git (add|commit|pop|remove|rename|restore|rm|stash|status|submodule)" -- "$argv"
+        return 1
+    end
+
+    # Ignore some `-` terminating args
+    if string match -qr "\-(v|V|h|H)\$" -- "$argv"
+        return 1
+    end
+
+    # Ignore some `--` terminating args
+    if string match -qr "\-\-(help)\$" -- "$argv"
+        return 1
+    end
+
+    return 0
+end
+
+# fzf
+set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
+
+function fzfp
+    fzf --layout='default' \
+        --ansi \
+        --preview-window=top,75%,sharp,wrap \
+        --bind 'focus:transform-header:file --brief {}' \
+        --bind='ctrl-d:abort' \
+        --bind='ctrl-s:change-preview(stat {})' \
+        --bind='ctrl-e:change-preview(bat -n --color=always {})' \
+        --bind='ctrl-w:toggle-preview'
+end
+
+# setup
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+starship init fish | source
+
+set -x FZF_DEFAULT_OPTS_FILE $XDG_CONFIG_HOME/fzf/default_ops
+fzf --fish | source
+
+zoxide init fish | source
+
+#direnv hook fish | source
