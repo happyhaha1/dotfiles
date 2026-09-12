c7dcf6aa0440b34aa01a734c3335cc781a905643
happyhaha1 <474248257@qq.com>
2026-09-12 22:59:13 +0800

    feat(pi): add DeepSeek V4.1 Flash

--- private_dot_config/private_fish/functions/aicommit.fish
+++ private_dot_config/private_fish/functions/aicommit.fish
@@ -1,4 +1,4 @@
-function aicommit --description 'Ask OpenCode to create a Git commit'
+function aicommit --description 'Ask Pi to create a Git commit'
     if not git rev-parse --git-dir >/dev/null 2>&1
         echo 'Not a git repository'
         return 1
@@ -10,5 +10,5 @@ function aicommit --description 'Ask OpenCode to create a Git commit'
         set prompt $argv
     end
 
-    opencode run "$prompt"
+    pi -p -- "$prompt"
 end
